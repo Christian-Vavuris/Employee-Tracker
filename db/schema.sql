c0f4fd3513de94292fc138408643280ce919c383
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-07 20:07:22 -0700

    added in my SQL tables and data but I can't get them to write to the database

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+-- Why wont these tables generate when I run "npm run migrate"
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR (30)
+);
+
+CREATE TABLE roles (
+    roles_id INTEGER PRIMARY KEY,
+    title VARCHAR (30), 
+    salary DECIMAL, 
+    department_id INTEGER 
+);
+
+CREATE TABLE employees (
+    employees_id INTEGER PRIMARY KEY, 
+    first_name VARCHAR (30),
+    last_name VARCHAR (30), 
+    role_id INTEGER, 
+    manager_id INTEGER 
+);
